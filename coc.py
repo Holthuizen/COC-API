910f6fc22ca8d24ca3ed6e8d18f822b6876e4f8a
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2021-03-04 13:59:50 +0100

    fix clanTag

--- coc.py
+++ coc.py
@@ -2,7 +2,7 @@
 import requests as request
 import json 
 
-
+#per day info of a clan-war-league
 class WarDay(): 
     def __init__(self,day,warTag,res): 
         self.day = day
@@ -11,14 +11,16 @@ class WarDay():
     
 
 class ClanInfo:
-    def __init__(self): 
+    def __init__(self,clantag): 
         self.base = "https://api.clashofclans.com/v1/"
         self.token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM5MzRmMjllLTA0Y2UtNGQ2ZS1iZDM1LWVmZmNlNDI4MWEzYiIsImlhdCI6MTYxNDY4NjcxNywic3ViIjoiZGV2ZWxvcGVyLzhlOTIyMmY4LWNjZDUtN2MyZi1mMDE5LWRhNjI0MjlhMjdhNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc3LjE2MS4xMjUuMTM1IiwiNS4xOTkuMTQ4LjIwMSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.aU5Oe-l_Way2quK62V1f25FuVuo_efwUfdnQXmZbkV6o-Sf8Ph5mAdGNd6EJ3WizgBklDnRHx0p4JN_80Nqlug"
         self.headers = {'Content-Type': 'application/json', 'Authorization': self.token}
         self.ownClanWars = []
+        self.clanTag = "%23"+clantag[1:]
 
 
     # get /clanwarleagues/wars/{warTag} 
+    #https://api.clashofclans.com/v1/clanwarleagues/wars/%232RL9C2CUG
     def clanwarleagues_wars(self,warTags):
         urlSaveWarTag = "%23"+warTags[1:]
         endpoint = self.base + f"clanwarleagues/wars/{urlSaveWarTag}"
@@ -28,26 +30,28 @@ class ClanInfo:
         else: 
             return res.status_code
 
-    def currentwar_leaguegroup(self, clantag): 
-        endpoint = self.base+"clans/"+clantag+"/currentwar/leaguegroup"
-
+    #https://api.clashofclans.com/v1/clans/%2329G2CU2JY/currentwar/leaguegroup
+    def currentwar_leaguegroup(self): 
+        endpoint = self.base+"clans/"+self.clanTag+"/currentwar/leaguegroup"
         r = request.get(endpoint, headers = self.headers)
         if r.status_code == 200: 
             return r.json()
 
-    def get_tags(self,clantag,day): 
+    #goes through currentwar_leaguegroup("war tag")
+    def get_tags(self,day): 
         tags = []
-        r = self.currentwar_leaguegroup("%2329G2CU2JY")
+        r = self.currentwar_leaguegroup()
         if r:   
             for warTag in r['rounds'][day]['warTags']: 
                 tags.append(warTag)
         return tags
 
-    def clan_war_league_event(self,clantag,days):
+    def clan_war_league_event(self,days):
+        
         tags = []
         for day in range(days):
             #print(day) 
-            tags = self.get_tags(clantag,day)
+            tags = self.get_tags(day)
             for warTag in tags: 
                 res = self.clanwarleagues_wars(warTag)
                 if res['clan']['tag'] == '#29G2CU2JY' or  res['opponent']['tag'] == '#29G2CU2JY':
@@ -63,8 +67,8 @@ class ClanInfo:
 
 
 
-claninfo = ClanInfo()
-claninfo.clan_war_league_event("%2329G2CU2JY",2)
+claninfo = ClanInfo("#29G2CU2JY")
+claninfo.clan_war_league_event(2)
 
 print(len(claninfo.ownClanWars))
 for war in claninfo.ownClanWars: 
